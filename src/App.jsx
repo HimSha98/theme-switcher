3b63670f998d11b9e5b26afdf3c901cf5b7d2597
Himanshu Sharma <himanshu.sharma.1013@gmail.com>
2026-05-19 23:03:37 +0530

    HS CREATED THEME SWITCHER

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme';
+import Card from './components/Card';
+import ThemeBtn from './components/ThemeBtn';
+import { useEffect } from 'react';
 
 function App() {
+    const [themeMode, setThemeMode] = useState('light');
 
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                {/* THEME BUTTON */}
-            </div>
+    const lightTheme = () => {
+        setThemeMode('light');
+    }
 
-            <div className="w-full max-w-sm mx-auto">
-                {/* CARD */}
+    const darkTheme = () => {
+        setThemeMode('dark');
+    }
+
+    // HS ACTUAL CHANGE IN THEME
+    useEffect(() => {
+        const doc = document.querySelector('html');
+
+        doc.classList.remove(themeMode === 'light' ? 'dark' : 'light');
+        doc.classList.add(themeMode);
+
+    }, [themeMode]);
+
+    return (
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn />
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                        <Card />
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+        </ThemeProvider>
+    )
 }
 
 export default App;
--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -5,3 +5,10 @@ export const ThemeContext = createContext({
     darkTheme: () => {},
     lightTheme: () => {},
 });
+
+export const ThemeProvider = ThemeContext.Provider; // HS Creating a provider for the theme context
+
+// HS Custom hook to use the theme context
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
